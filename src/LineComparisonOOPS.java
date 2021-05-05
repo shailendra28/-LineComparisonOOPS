9ab2c7cf0b269a46effe2ea02f7db7cd2cdca0ca
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-05 09:41:12 +0530

    ADDED: Compare Two Lines

--- src/LineComparisonOOPS.java
+++ src/LineComparisonOOPS.java
@@ -10,15 +10,13 @@ class lineFunction{
                 double y = Math.pow((y2 - y1), 2);
                 return Math.sqrt(x + y);
         }
-        boolean isEqual(double length , double length1){
-                if(String.valueOf(length).equals(String.valueOf(length1))){
-                        return true;
-                }
-                else{
-                        return false;
-                }
-        }
+
+	int compaire(double length , double length1){
+		int result = Double.valueOf(length).compareTo(Double.valueOf(length1));
+		return result;
+	}
 }
+   
 public class LineComparisonOOPS {
 
 	public static void main(String[] args) {
@@ -34,12 +32,15 @@ public class LineComparisonOOPS {
         int y12 = lineFunctionObject.getLineValue();
 		double length = lineFunctionObject.getLineLength(x1,x2,y1,y2);
 		double length1 = lineFunctionObject.getLineLength(x11 , x12 , y11 , y12);
-		boolean isEqual = lineFunctionObject.isEqual(length , length1);
-		if(isEqual == true){
-			System.out.println("Lines are equal!");
+		int compaire = lineFunctionObject.compaire(length , length1);
+		if(compaire >= 1){
+			System.out.println("line1 is greater than line2");
+		}
+		else if(compaire < 0){
+			System.out.println("line1 is less than line2");
 		}
 		else{
-			System.out.println("Lines are not equal!");
+			System.out.println("Lines are equal!");
 		}
 	}
 }
